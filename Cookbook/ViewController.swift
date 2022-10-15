9cd34cc90010ca4612f624987bfcd02cd24860f7
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-10-15 23:02:38 +0300

    feat: create new project without Storyboard file

---
+++ Cookbook/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Cookbook
+//
+//  Created by Егор Бадмаев on 15.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
