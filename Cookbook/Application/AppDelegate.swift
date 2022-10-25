1b94ef4e8997bec4e3ce41aaddb1988cdc7545da
Egor Badmaev <eg.badmaev@gmail.com>
2022-10-25 19:03:51 +0300

    refactor: set min. version of iOS to 12.0

---
+++ Cookbook/Application/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Cookbook
+//
+//  Created by Егор Бадмаев on 15.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        
+        return true
+    }
+}
