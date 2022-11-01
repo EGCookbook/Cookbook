c931fc57d4ded5d8bb1b05b40e5473dc6f41846f
Egor Badmaev <eg.badmaev@gmail.com>
2022-11-01 23:45:13 +0300

    feat: add `AppCoordinator` tests

---
+++ CookbookTests/Application/AppCoordinatorTests.swift
@@ -0,0 +1,32 @@
+//
+//  AppCoordinatorTests.swift
+//  CookbookTests
+//
+//  Created by Егор Бадмаев on 01.11.2022.
+//
+
+import XCTest
+@testable import Cookbook
+
+class AppCoordinatorTests: XCTestCase {
+    
+    var window: UIWindow!
+    var appCoordinator: AppCoordinator!
+    
+    override func setUpWithError() throws {
+        window = UIWindow(frame: .zero)
+        appCoordinator = AppCoordinator(window: window)
+    }
+    
+    override func tearDownWithError() throws {
+        window = nil
+        appCoordinator = nil
+    }
+    
+    /// This test simulates start of the application and checks that there is new view controller in `window.rootViewController`.
+    func testExample() throws {
+        XCTAssertNoThrow(appCoordinator.start())
+        
+        XCTAssertNoThrow(window.rootViewController)
+    }
+}
