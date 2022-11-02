4963b076af922294413753d26200d924689f82fe
Egor Badmaev <eg.badmaev@gmail.com>
2022-11-02 21:01:22 +0300

    refactor: replace Resources from module to core

---
+++ Cookbook/Resources/Localization/Texts.swift
@@ -0,0 +1,22 @@
+//
+//  Texts.swift
+//  Resources
+//
+//  Created by Егор Бадмаев on 27.10.2022.
+//
+
+public enum Texts {
+    public enum Discover {
+        public static let title = "discover".localized
+        public static let dishOfTheDay = "dishOfTheDay".localized
+        public static let mockRecipeTitle = "mockRecipeTitle".localized
+    }
+    
+    public enum Search {
+        public static let title = "search".localized
+    }
+    
+    public enum Personal {
+        public static let title = "personal".localized
+    }
+}
