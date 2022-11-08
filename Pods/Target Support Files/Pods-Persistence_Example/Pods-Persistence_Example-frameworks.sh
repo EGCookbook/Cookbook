d063ef74bc999c3fcac61cf9a6277e9bd7067925
Egor Badmaev <eg.badmaev@gmail.com>
2022-11-08 23:55:03 +0300

    refactor: commit `RecipeDetails` release

--- Pods/Target Support Files/Pods-Persistence_Example/Pods-Persistence_Example-frameworks.sh
+++ Pods/Target Support Files/Pods-Persistence_Example/Pods-Persistence_Example-frameworks.sh
@@ -176,9 +176,11 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Models/Models.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Persistence/Persistence.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Models/Models.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Persistence/Persistence.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
